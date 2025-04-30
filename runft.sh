94488378d3a8bd1c2ed80bbf0eb6d35aa275b12f
Ju-In Hum <juinhummail@gmail.com>
2025-04-30 14:06:18 -0700

    delete space bugging out runft

--- runft.sh
+++ runft.sh
@@ -22,6 +22,6 @@ python main.py  \
     --mix_type 1 \
     --repeat 1 \
     --relabeled forseverity \
-    --unfreezing classifieronly \ 
+    --unfreezing classifieronly \
     --save False \
     --train_sample_cells=500  --test_sample_cells=500  --train_num_sample=20 --test_num_sample=50
