0a1fe3f5aba398dfb0d704508dd102831b7cdecc
Ju-In Hum <juinhummail@gmail.com>
2025-04-29 17:53:51 -0700

    cut down custom.sh

--- custom.sh
+++ custom.sh
@@ -1,24 +1,9 @@
 python custom.py  \
-    --model Transformer \
-    --epochs 100 \
-    --norm_first False \
-    --seed 100 \
     --task haniffa \
-    --all 0 \
-    --h_dim 128 \
+    --relabeled True \
+    --pca True \
     --heads 8 \
     --layers 1 \
     --dropout 0.3 \
-    --min_size 10000 \
-    --inter_only True \
-    --batch_size 256 \
-    --same_pheno -1 \
-    --n_splits 2 \
-    --augment_num 300 \
-    --pca True \
-    --warmup False \
-    --learning_rate 0.01 \
-    --alpha 0.5 \
-    --mix_type 1 \
-    --repeat 1 \
-    --train_sample_cells=500  --test_sample_cells=500  --train_num_sample=20 --test_num_sample=50
+    --norm_first False \
+    --test_sample_cells=500 --test_num_sample=50
